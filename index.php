056976b3a3e9b738b5619c70b4fdd07a69c2b3f8
Andron96 <drandarik2@gmail.com>
2023-12-18 20:26:06 +0200

    3

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types=1);
 
-function fibonacciNumbers($length)
+function fibonacciNumbers(int $length): iterator
 {
-    $fN = 0;
     $fN1 = 1;
     $fN2 = 1;
     for ($i = 1; $i <= $length; $i++) {
-        if ($i <= 2) {
+        if ($i <= 1) {
+            $fN = 0;
+        } elseif ($i <= 3) {
             $fN = 1;
         } else {
             $fN = $fN1 + $fN2;
@@ -19,6 +20,11 @@ function fibonacciNumbers($length)
 }
 
 $length = (int) readline("Введіть довжину: ");
-foreach (fibonacciNumbers($length) as $value) {
-    echo "value: $value\n";
+echo "Згенерована послідовність Фібоначчі: ";
+foreach (fibonacciNumbers($length) as $key => $value) {
+    if ($key === array_key_last(iterator_to_array(fibonacciNumbers($length)))) {
+        echo "$value.";
+    } else {
+        echo "$value; ";
+    }
 }
