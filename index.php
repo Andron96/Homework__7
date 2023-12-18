cd42a068309deab1f791b5f2a7b1ae2157e71e14
Andron96 <drandarik2@gmail.com>
2023-12-18 21:52:25 +0200

    4

--- index.php
+++ index.php
@@ -1,30 +1,36 @@
 <?php
 declare(strict_types=1);
 
-function fibonacciNumbers(int $length): iterator
+function fibonacciNumbers(int $maxVal): iterator
 {
+    $fN = 0;
     $fN1 = 1;
     $fN2 = 1;
-    for ($i = 1; $i <= $length; $i++) {
-        if ($i <= 1) {
-            $fN = 0;
-        } elseif ($i <= 3) {
+    for ($i = 1; ($fN <= $maxVal) && ($maxVal != 0); $i++) {
+        if (($i > 1) && ($i <= 3)) {
             $fN = 1;
-        } else {
+        } elseif ($i > 3) {
             $fN = $fN1 + $fN2;
         }
         $fN2 = $fN1;
         $fN1 = $fN;
-        yield $fN;
+        if ($fN <= $maxVal) {
+            yield $fN;
+        } else {
+            break;
+        }
     }
 }
 
-$length = (int) readline("Введіть довжину: ");
+$maxVal = (int) readline("Введіть максимальне значення послідовності Фібоначчі (від \"1\" і вище): ");
 echo "Згенерована послідовність Фібоначчі: ";
-foreach (fibonacciNumbers($length) as $key => $value) {
-    if ($key === array_key_last(iterator_to_array(fibonacciNumbers($length)))) {
+foreach (fibonacciNumbers($maxVal) as $key => $value) {
+    if ($key === array_key_last(iterator_to_array(fibonacciNumbers($maxVal)))) {
         echo "$value.";
     } else {
         echo "$value; ";
     }
+}
+if ($maxVal <= 0) {
+    echo "Ви ввели неправильне максимальне значення.";
 }
